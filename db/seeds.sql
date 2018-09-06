cb2bcc477ee769b23ebab662909d69e9ffd72180
Matthew Tai <matthewtai@gmail.com>
2018-09-06 01:26:50 -0400

    burger

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+-- Creating seed data for the burgers_db database --
+INSERT INTO burgers (burger_name, devoured) VALUES ('Baconator', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Quarter Pounder', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Animal Style', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Papa Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Ass Burgers', true);
+
